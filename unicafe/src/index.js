7db147d1b119f79d2d88115a861ad8e07f74561f
h8557 <harry@unicus.com>
2019-12-19 13:25:54 +0200

    Started writing Unicafe part 1

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Feedback = () => {
+    return (
+        <div>
+            <h1>give feedback</h1>
+        </div>
+    )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const App = () => {
+ 
+
+    return (
+        <div>
+            <Feedback />
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
